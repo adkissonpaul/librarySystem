c6783cb365efba60cb5e02279032626cc31187cd
Paul <68358717+adkissonpaul@users.noreply.github.com>
2020-10-01 13:34:15 -0400

    Update librarySystem.js

--- librarySystem.js
+++ librarySystem.js
@@ -1,32 +1,31 @@
 (function() {
 	
-	var libraryStorage = {};
+  var libraryStorage = {};
 
-	function librarySystem(libraryName, array, callback) {
+  function librarySystem(libraryName, array, callback) {
     
-    if(arguments.length === 1){
-    	var todaysLibrary = libraryStorage[libraryName];
-			var dependencyArray = [];
+  if(arguments.length === 1){
+    var todaysLibrary = libraryStorage[libraryName];
+    var dependencyArray = [];
 
-    	if(todaysLibrary.array.length > 0) {
-				
-				dependencyArray = todaysLibrary.array.map(function(val) {
-					return libraryStorage[val].callback();
-				});
+    if(todaysLibrary.array.length > 0) {
+      dependencyArray = todaysLibrary.array.map(function(val) {
+        return libraryStorage[val].callback();
+	  });
 
-				return todaysLibrary.callback.apply(this, dependencyArray);
+	return todaysLibrary.callback.apply(this, dependencyArray);
 			
-			} else {
-					return todaysLibrary.callback();
-				}
+     } else {
+         return todaysLibrary.callback();
+     }
 
-			} else if (arguments.length > 1) {
-    		libraryStorage[libraryName] = {
-    			callback: callback,
-    			array: array
-    		}
-    	}
+  } else if (arguments.length > 1) {
+      libraryStorage[libraryName] = {
+    	callback: callback,
+    	array: array
+      }
     }
+  }
 
   window.librarySystem = librarySystem;
 
